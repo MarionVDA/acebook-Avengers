6fb74a42f9855c76be8980f7a5c97c33b0b710d5
Marion <marion.vagnerdamicourt@kpmg.co.uk>
2019-11-05 14:42:19 +0000

    fix rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,13 +4,13 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  default_scope {order(created_at: :asc)}
+  default_scope { order(created_at: :asc) }
 
   def editable?
-   if (Time.now - self.created_at < 10.minutes)
-     return "true"
-   else
-     return "false"
-   end
- end
+    if Time.now - created_at < 10.minutes
+      'true'
+    else
+      'false'
+    end
+  end
 end
